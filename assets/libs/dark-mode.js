92c5e1c3114f0f0dada678383ba8bbf6ecffebb8
AlgoTechniq <126805427+algotechniq@users.noreply.github.com>
2023-05-06 08:52:19 -0600

    Create dark-mode.js

---
+++ assets/libs/dark-mode.js
@@ -0,0 +1,21 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+function setTheme(theme) {
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.removeAttribute('data-theme');
+  }
+  localStorage.setItem('theme', theme);
+}
+
+themeToggle.addEventListener('click', () => {
+  const currentTheme = localStorage.getItem('theme') || 'light';
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  setTheme(newTheme);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme') || 'light';
+  setTheme(savedTheme);
+});
